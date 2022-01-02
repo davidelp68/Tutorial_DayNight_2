17e0468056d8d5c6cbc6a28d09437dad34cd5e93
davidelp68 <davide.malu@live.it>
2022-01-02 15:24:29 +0100

    Initial commit

---
+++ app/src/main/java/com/dm/tutorialdaynight2/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.dm.tutorialdaynight2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatDelegate     //Importazione della Classe AppCompactDelegate
+
+class MainActivity : AppCompatActivity() {
+
+    private var rbDay: RadioButton? = null
+    private var rbNight: RadioButton? = null
+    private var rbDefault: RadioButton? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rbDay = findViewById(R.id.rB_day)
+        rbNight = findViewById(R.id.rB_night)
+        rbDefault = findViewById(R.id.rB_default)
+
+
+        rbDay!!.setOnClickListener {
+            //Imposta il tema chiaro
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        rbNight!!.setOnClickListener {
+            //Imposta il tema scuro
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        rbDefault!!.setOnClickListener {
+            //Imposta il tema in base all'impostazione di sistema
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+
+
+    }
+}
